6a7da6cbad6459e14343e809be42d5187612ce17
groMMaster <Grigory.Vinokursky@urfu.me>
2022-11-15 15:36:57 +0500

    New repository

---
+++ GreatSolver/GreatSolver/Equations/Roots.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatSolver.Equations
+{
+    //Об этом еще подумать
+    public struct Roots
+    {
+        public bool IsAny { get; }
+        public double[] Value { get; }
+        public Roots(bool isAny)
+        {
+            IsAny = isAny;
+            Value = null;
+        }
+
+        public Roots(params double[] value)
+        {
+            IsAny = false;
+            Value = value;
+        }
+    }
+}
---
+++ GreatSolver/GreatSolver/Equations/Types/Line.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatSolver.Equations.Types
+{
+    public class Line : IEquation
+    {
+        public double A;
+        public double B;
+
+        public Line(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public Roots GetRoots()
+        {
+            if (A == 0)
+            {
+                if (B == 0) return new Roots(isAny: true);
+                throw new Exception();
+            }
+
+            return new Roots(B / A);
+        }
+    }
+}
